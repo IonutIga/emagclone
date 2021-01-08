b05c1d1d2db3ce39f8910e97c0940c157b33e769
VASILE-IONUȚ-REMUS IGA <vasile.iga@stud.ubbcluj.ro>
2021-01-08 12:50:23 +0200

    fix format

--- lib/src/epics/app_epics.dart
+++ lib/src/epics/app_epics.dart
@@ -4,18 +4,16 @@ import 'package:emag/src/models/index.dart';
 import 'package:meta/meta.dart';
 import 'package:redux_epics/redux_epics.dart';
 
-class AppEpics{
-
-  const AppEpics({@required AuthApi authApi}):assert(authApi != null),
+class AppEpics {
+  const AppEpics({@required AuthApi authApi})
+      : assert(authApi != null),
         _authApi = authApi;
 
   final AuthApi _authApi;
 
-  Epic<AppState> get epics{
+  Epic<AppState> get epics {
     return combineEpics(<Epic<AppState>>[
       AuthEpics(api: _authApi).epics,
-
     ]);
   }
-
 }
